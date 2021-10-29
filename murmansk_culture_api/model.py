8fdca74c16f17cc9b57b673a024fce2e4c0955d8
Arthur Orlovsky <an.orlovskiy@gmail.com>
2021-10-29 18:00:46 +0300

    rename project and change configs accordingly (yet again)

--- murmansk_culture_api/model.py
+++ murmansk_culture_api/model.py
@@ -52,14 +52,14 @@ class Data:
         return self
 
     def scrap_and_save(self):
-        # self.current_exhibitions = scrap_artmuseum(
-        #     TimeLabel.NOW, {exh.url: exh.address for exh in self.current_exhibitions}
-        # )
-        # self.upcoming_exhibitions = scrap_artmuseum(
-        #     TimeLabel.SOON, {exh.url: exh.address for exh in self.upcoming_exhibitions}
-        # )
-        self.current_exhibitions = scrap_artmuseum(TimeLabel.NOW, scrap_addrs=False)
-        self.upcoming_exhibitions = scrap_artmuseum(TimeLabel.SOON, scrap_addrs=False)
+        self.current_exhibitions = scrap_artmuseum(
+        TimeLabel.NOW, scraped_addrs={exh.url: exh.address for exh in self.current_exhibitions}
+        )
+        self.upcoming_exhibitions = scrap_artmuseum(
+        TimeLabel.SOON, scraped_addrs={exh.url: exh.address for exh in self.upcoming_exhibitions}
+        )
+        # self.current_exhibitions = scrap_artmuseum(TimeLabel.NOW, scrap_addrs=False)
+        # self.upcoming_exhibitions = scrap_artmuseum(TimeLabel.SOON, scrap_addrs=False)
         self.concerts = scrap_philharmonia()
 
         logging.info("Scraped data")
