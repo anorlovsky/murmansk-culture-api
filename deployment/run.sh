b913cb0b5ea21b7b01a1ada560d400322ac4f56d
Arthur Orlovsky <an.orlovskiy@gmail.com>
2021-10-29 11:18:22 +0300

    rename tests
    I tried the "mirror your src tree" approach for unit tests, but flat structure (which I discovered by looking at fastapi tests) seems much more readable and avoids the problem of duplicating effort if I change something in the src tree.
    I think that the primary way of knowing how much code is untested should be code coverage tools, not the src-mirroring convention.

--- deployment/run.sh
+++ deployment/run.sh
@@ -3,7 +3,7 @@
 script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 PROXY_FLAGS="--proxy-headers --forwarded-allow-ips='*'"
-ROOT_PATH="--root-path /artmuseum/"
+ROOT_PATH="--root-path /murmansk-culture/api/"
 
 cd "$script_path"/murmansk_culture
-../env/bin/python -m uvicorn main:app --port 8000 --log-config logging.yaml $PROXY_FLAGS $ROOT_PATH
+../env/bin/python -m uvicorn main:app --port 8000 --log-config log_config.yaml $PROXY_FLAGS $ROOT_PATH &
