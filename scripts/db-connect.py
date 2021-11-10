b6075418a00c39890f2eeb591631dc3cb22e0ac1
Arthur Orlovsky <an.orlovskiy@gmail.com>
2021-11-10 17:01:47 +0300

    show logging.INFO in db-connect script

--- scripts/db-connect.py
+++ scripts/db-connect.py
@@ -1,10 +1,14 @@
 """This script is used for interactively connecting to the database with ipython: 
   `ipython -i scripts/db-connect.py`"""
+import logging
 
 from sqlmodel import Session, create_engine, select
 
+from murmansk_culture_api.db.crud import refresh_data
 from murmansk_culture_api.db.models import ArtmuseumExhibition
 
+
+logging.getLogger().setLevel(logging.INFO)
 
 engine = create_engine(
     "sqlite:///database.db", connect_args={"check_same_thread": False}
