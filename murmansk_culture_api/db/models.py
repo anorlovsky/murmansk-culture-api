55c91890ba3d90f9f2a6e69738381cc35fce16e5
Arthur Orlovsky <an.orlovskiy@gmail.com>
2021-11-11 17:07:26 +0300

    use pydantic.HttpUrl type over plain str

--- murmansk_culture_api/db/models.py
+++ murmansk_culture_api/db/models.py
@@ -1,17 +1,18 @@
 from datetime import date, datetime
 from typing import Optional
 
+from pydantic import HttpUrl
 from sqlmodel import Field, SQLModel
 
 from ..datatypes import ArtmuseumAddress, ArtmuseumTimeLabel
 
 
-# TODO: use pydantic's "URL" type
+# TODO: exclude id's from pydantic models (let's do it here, not in the fastapi routing code)
 class PhilharmoniaConcert(SQLModel, table=True):
     # __tablename__ = "..."
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
-    url: str
+    url: HttpUrl
     description: str
     date: datetime
     pushkin_card: bool = Field(
@@ -24,7 +25,7 @@ class PhilharmoniaConcert(SQLModel, table=True):
 class ArtmuseumExhibition(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
-    url: str
+    url: HttpUrl
     start_date: date
     end_date: Optional[date] = None
     address: Optional[ArtmuseumAddress] = None
